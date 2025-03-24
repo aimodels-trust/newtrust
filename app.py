ac2db9b90cdab5895e533a0a4a51391e1f02dc77
aimodels-trust <sdpfinalbusiness@gmail.com>
2025-03-24 12:05:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import shap
 import matplotlib.pyplot as plt
 
 # Step 1: Download the model from Google Drive
-model_url = "https://drive.google.com/uc?id=1en2IPj_z6OivZCBNDXepX-EAiZLvCILE"
+model_url = "https://drive.google.com/uc?id=1Re7B6ANRMotj6LKOvwVWBfuSSCJyBiqY"
 model_path = "credit_default_model.pkl"
 
 # Check if the model file already exists; if not, download it
